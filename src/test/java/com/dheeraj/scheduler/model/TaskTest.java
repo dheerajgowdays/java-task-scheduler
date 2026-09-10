397fd251789f82294e96948417e92bc46234697b
Dheeraj Gowda YS <dheeraj76766@gmail.com>
2026-09-10 22:49:50 +0530

    .

--- src/test/java/com/dheeraj/scheduler/model/TaskTest.java
+++ src/test/java/com/dheeraj/scheduler/model/TaskTest.java
@@ -8,13 +8,53 @@ import java.time.Instant;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TaskTest {
+    private Task  createTask(){
+        return new Task(UUID.randomUUID(),"task1","task",()->System.out.println("task"), TaskPriority.HIGH);
+    }
     @Test
     void shouldCreateTask(){
-        Task task = new Task(UUID.randomUUID(),"task1","task",()->System.out.println("task"), TaskPriority.HIGH);
-        t
+        Task task = createTask();
         assertEquals(TaskStatus.CREATED,task.getStatus());
+        task.schedule(Instant.now().plusSeconds(10000));
+        assertEquals(TaskStatus.SCHEDULED,task.getStatus());
         assertEquals("task1",task.getName());
+    }
+    @Test
+    void shouldCreateStartComplete(){
+        Task task = createTask();
+        task.schedule(Instant.now().plusSeconds(10000));
+        assertEquals(TaskStatus.SCHEDULED,task.getStatus());
+        task.start();
+        assertEquals(TaskStatus.RUNNING,task.getStatus());
+        task.complete();
+        assertEquals(TaskStatus.COMPLETED,task.getStatus());
+    }
+    @Test
+    void shouldCreateCancel(){
+        Task task = createTask();
+        assertEquals(TaskStatus.CREATED,task.getStatus());
+        task.cancel();
+        assertEquals(TaskStatus.CANCELLED,task.getStatus());
+    }
+    @Test
+    void shouldCompleteStart(){
+        Task task = createTask();
+        task.schedule(Instant.now().plusSeconds(10000));
+        task.start();
+        task.complete();
+        assertThrows(IllegalStateException.class, task::start);
+    }
+    @Test
+    void shouldSchedulePast(){
+        Task task =createTask();
+        assertThrows(IllegalArgumentException.class,()->task.schedule(Instant.parse("2026-06-08T14:30:00Z")));
+    }
+    @Test
+    void shouldScheduleNull(){
+        Task task =createTask();
+        assertThrows(IllegalArgumentException.class,()->task.schedule(null));
     }
 }
